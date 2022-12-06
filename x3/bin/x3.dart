684217363e1027e6039b579f2ef03d29b8166298
HuzeyfeHalil <117576180+HuzeyfeHalil@users.noreply.github.com>
2022-12-06 22:50:25 +0300

    Add files via upload

--- x3/bin/x3.dart
+++ x3/bin/x3.dart
@@ -1,31 +1,4 @@
 void main() {
-  // List M = ["*", "**", "***", "****", "*****"];
-  // for (int q = 0; q < M.length; q++) {
-  //   print(M[q]);
-  // }
-  // List<int> l = [1, 10, 15, 20, 25];
-  // List<int> Leven = [];
-  // List<int> Lodd = [];
-  // int y = 0, h = 0, h1 = 0;
-  // int y1 = 0;
-  // print(l);
-  // for (int i = 0; i < l.length; i++) {
-  //   if (l[i] % 2 == 0) {
-  //     Leven.add(l[i]);
-  //     if (Leven[h] > i) {
-  //       h1 = Leven[h];
-  //       h++;
-  //     }
-  //   } else {
-  //     Lodd.add(l[i]);
-  //     if (Lodd[y] < i) {
-  //       y1 = Lodd[y];
-  //       y++;
-  //     }
-  //   }
-  // }
-  // print("$Leven $Lodd");
-  // print("$y $y1");
   List<int> l = [1, 14, 21, 31, 18, 90];
   List<int> Leven = [];
   List<int> Lodd = [];
